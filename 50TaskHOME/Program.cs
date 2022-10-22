58c59bbeb18fe17270fb20448d4f3438af7e6b32
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-22 15:08:18 +0300

    50taskHOME

---
+++ 50TaskHOME/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 50. Напишите программу, которая на вход
+ принимает позиции элемента в двумерном массиве, 
+ и возвращает значение этого элемента или же указание,
+  что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Введено не число. Повторите ввод.");
+    return result;
+}
+
+int[,] InitArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = rnd.Next(2, 10);
+        }
+    }
+    return newArray;
+}
+int Numbtrue(int[,] matrix, int num)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == num)
+            {
+                Console.WriteLine("Такое число есть");
+                return num;
+            }
+        }
+    }
+    Console.WriteLine("Такого числа нет");
+    return num;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = GetNumber("Введите количество строк: ");
+int column = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitArray(row, column);
+PrintMatrix(matrix);
+Console.WriteLine("Введите число для сравнения: ");
+int num = int.Parse(Console.ReadLine());
+Numbtrue(matrix, num);
